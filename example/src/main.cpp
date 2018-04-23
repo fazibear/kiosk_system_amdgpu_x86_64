2d7fea3d20afaf62625f1ceb4d9f0f3b10892cc3
Michał Kalbarczyk <fazibear@gmail.com>
2018-04-23 16:51:12 +0200

    Add kms conf

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -41,6 +41,8 @@ int main(int argc, char **argv)
 {
  qputenv("QT_LOGGING_RULES", "qt.qpa.*=true");
  qputenv("QSG_INFO", "1");
+ qputenv("QSG_INFO", "1");
+ qputenv("QT_QPA_EGLFS_KMS_CONFIG", "/etc/kms.json");
 
  QApplication app(argc, argv);
 
