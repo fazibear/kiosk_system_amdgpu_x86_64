5cfcc7f6413bb1a73194d12e0fcc8325e84880e1
Michał Kalbarczyk <fazibear@gmail.com>
2018-04-23 09:59:27 +0200

    Try testapp

---
+++ example/qt-webengine-kiosk.pro
@@ -0,0 +1,13 @@
+TEMPLATE = app
+
+QT += quick
+
+SOURCES = src/main.cpp
+RESOURCES = src/quickmwtest.qrc
+OTHER_FILES = src/screen.qml
+
+DESTDIR = priv/
+OBJECTS_DIR = _build/qt
+MOC_DIR = _build/qt
+RCC_DIR = _build/qt
+UI_DIR = _build/qt
--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,117 +1,53 @@
-/****************************************************************************
-**
-** Copyright (C) 2011 Sergey Dryabzhinsky
-** All rights reserved.
-** Contact: Sergey Dryabzhinsky (sergey.dryabzhinsky@gmail.com)
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights.  These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at qt-info@nokia.com.
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+#include <QGuiApplication>
+#include <QQuickView>
+#include <QQmlEngine>
+#include <QQmlContext>
+#include <QScreen>
+#include <QDebug>
 
-#include <QApplication>
-#include "mainwindow.h"
-
-
-
-int main(int argc, char * argv[])
+static QQuickView *addView(QScreen *screen, int screenIdx)
 {
-    qDebug() << "Start!!!!";
+    qDebug("Creating new QQuickView for screen %p", screen);
+    QQuickView *v = new QQuickView;
 
+    v->setScreen(screen);
+    v->setResizeMode(QQuickView::SizeRootObjectToView);
+
+    v->rootContext()->setContextProperty("screenIdx", screenIdx);
+    v->rootContext()->setContextProperty("screenGeom", screen->geometry());
+    v->rootContext()->setContextProperty("screenAvailGeom", screen->availableGeometry());
+    v->rootContext()->setContextProperty("screenVirtGeom", screen->virtualGeometry());
+    v->rootContext()->setContextProperty("screenAvailVirtGeom", screen->availableVirtualGeometry());
+    v->rootContext()->setContextProperty("screenPhysSizeMm", screen->physicalSize());
+    v->rootContext()->setContextProperty("screenRefresh", screen->refreshRate());
+
+    v->setSource(QUrl("qrc:/screen.qml"));
+
+    QObject::connect(v->engine(), &QQmlEngine::quit, qGuiApp, &QCoreApplication::quit);
+
+    return v;
+}
+
+int main(int argc, char **argv)
+{
     qputenv("QT_LOGGING_RULES", "qt.qpa.*=true");
     qputenv("QSG_INFO", "1");
-    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
 
-    QStringList qargv;
-    for (int i=0; i<argc; i++) {
-        qargv.append(QString(argv[i]));
+    QGuiApplication app(argc, argv);
+
+    QList<QScreen *> screens = app.screens();
+    qDebug("Application sees %d screens", screens.count());
+    qDebug() << screens;
+
+    QVector<QQuickView *> views;
+    for (int i = 0; i < screens.count(); ++i) {
+        QQuickView *v = addView(screens[i], i);
+        views.append(v);
+        v->showFullScreen();
     }
 
-    //QApplication::setApplicationName("Qt WebEngine Kiosk");
+    int r = app.exec();
 
-    QCommandLineParser parser;
-    parser.setApplicationDescription("A kiosk browser based on Qt's Chromium-derived WebEngine component");
-    parser.addHelpOption();
-    parser.addVersionOption();
-
-    QStringList glModes;
-    QMetaEnum glModeEnum = MainWindow::staticMetaObject.enumerator(MainWindow::staticMetaObject.indexOfEnumerator("GlMode"));
-    for (int value = 0; value < glModeEnum.keyCount(); value++) {
-        glModes.append(QString(glModeEnum.valueToKey(value)));
-    }
-
-    QList<QCommandLineOption> options = QList<QCommandLineOption>({
-            {{"clear-cache","C"}, "Clear cached request data"},
-            {{"uri","u"}, "Set starting url to <uri>", "uri"},
-            {{"monitor", "m"}, "Display window on the <n>th monitor", "n"},
-            {{"config", "c"}, "Use <file> to configure this instance of qt-webengine-kiosk", "file"},
-            {"opengl", QString()+"Use <"+glModes.join('|')+"> for hardware accelerated rendering", "system"}
-            });
-
-    parser.addOptions(options);
-
-    parser.parse(qargv);
-    qDebug() << parser.errorText();
-    QString glMode;
-    if ((glMode = parser.value("opengl")).isEmpty())
-        glMode = "auto";
-    switch (glModeEnum.keyToValue(glMode.toUpper().toLatin1())) {
-    case MainWindow::NATIVE:
-        QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
-        qDebug() << "attempting to use native OpenGL";
-        break;
-    case MainWindow::ANGLE:
-        QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
-        qDebug() << "attempting to use OpenGL ES (or ANGLE, on Windows)";
-        break;
-    case MainWindow::SOFTWARE:
-        qDebug() << "using software rendering";
-        QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
-        break;
-    case MainWindow::AUTO:
-    default:
-        qDebug() << "using automatic gl";
-        break;
-    }
-    QCoreApplication::setAttribute(Qt::AA_Use96Dpi);
-    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-    QApplication app(argc, argv);
-    parser.process(app);
-
-    qDebug() << "Hey!!!!";
-
-    MainWindow *browser = new MainWindow();
-    browser->init(parser);
-
-    // executes browser.cleanupSlot() when the Qt framework emits aboutToQuit() signal.
-    QObject::connect(qApp, SIGNAL(aboutToQuit()),
-                     browser, SLOT(cleanupSlot()));
-
-    int ret = app.exec();
-    qDebug() << "Application return:" << ret;
-
+    qDeleteAll(views);
+    return r;
 }
